9ff2ea131b0a41a9725b762b460eea2b297cdb0b
Teruaki Kinoshita <teru@kinoshita.yokohama>
2025-11-09 13:34:27 +0900

    fix test

--- test/flutter_secure_dotenv_generator_test.dart
+++ test/flutter_secure_dotenv_generator_test.dart
@@ -1,12 +1,14 @@
+import 'package:analyzer/dart/analysis/session.dart';
 import 'package:analyzer/dart/constant/value.dart';
+import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
+import 'package:build/build.dart';
 import 'package:flutter_secure_dotenv_generator/src/annotation_generator.dart';
 import 'package:flutter_secure_dotenv_generator/src/environment_field.dart';
 import 'package:flutter_secure_dotenv_generator/src/helpers.dart';
-import 'package:test/test.dart';
-import 'package:analyzer/dart/element/element.dart';
-import 'package:build/build.dart';
+import 'package:pub_semver/src/version.dart';
 import 'package:source_gen/source_gen.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('FlutterSecureDotEnvAnnotationGenerator', () {
@@ -23,13 +25,16 @@ void main() {
     });
 
     test('should throw exception if element is not a class', () async {
-      final element = FunctionElementImpl('testFunction');
+      final element = TopLevelFunctionElementImpl('testFunction');
       final annotation = ConstantReader(null);
       final buildStep = BuildStepMock();
 
       expect(
         () => generator.generateForAnnotatedElement(
-            element, annotation, buildStep),
+          element,
+          annotation,
+          buildStep,
+        ),
         throwsException,
       );
     });
@@ -37,8 +42,12 @@ void main() {
 
   group('EnvironmentField', () {
     test('should create an instance of EnvironmentField', () {
-      final field = EnvironmentField('name', 'nameOverride',
-          DartTypeImpl('String'), DartObjectImpl('defaultValue'));
+      final field = EnvironmentField(
+        'name',
+        'nameOverride',
+        DartTypeImpl('String'),
+        DartObjectImpl('defaultValue'),
+      );
 
       expect(field.name, 'name');
       expect(field.nameOverride, 'nameOverride');
@@ -63,14 +72,19 @@ class BuildStepMock implements BuildStep {
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-class FunctionElementImpl implements FunctionElement {
+class MetadataImpl implements Metadata {
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+class TopLevelFunctionElementImpl implements TopLevelFunctionElement {
   @override
   final String name;
 
-  FunctionElementImpl(this.name);
+  TopLevelFunctionElementImpl(this.name);
 
   @override
-  List<ElementAnnotation> get metadata => [];
+  Metadata get metadata => MetadataImpl();
 
   @override
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
@@ -83,7 +97,7 @@ class ClassElementImpl implements ClassElement {
   ClassElementImpl(this.name);
 
   @override
-  List<ElementAnnotation> get metadata => [];
+  Metadata get metadata => MetadataImpl();
 
   @override
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
@@ -117,7 +131,7 @@ class FieldElementImpl implements FieldElement {
   FieldElementImpl(this.name, this.type);
 
   @override
-  List<ElementAnnotation> get metadata => [];
+  Metadata get metadata => MetadataImpl();
 
   @override
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
@@ -130,16 +144,22 @@ class InterfaceElementImpl implements InterfaceElement {
   InterfaceElementImpl(this.name);
 
   @override
-  List<ElementAnnotation> get metadata => [];
+  Metadata get metadata => MetadataImpl();
 
   @override
   List<InterfaceType> get allSupertypes => [];
 
   @override
-  List<PropertyAccessorElement> get accessors => [
-        PropertyAccessorElementImpl('accessor1', false),
-        PropertyAccessorElementImpl('accessor2', false),
-      ];
+  List<GetterElement> get getters => [
+    GetterElementImpl('getter1'),
+    GetterElementImpl('getter2'),
+  ];
+
+  @override
+  List<SetterElement> get setters => [
+    SetterElementImpl('setter1'),
+    SetterElementImpl('setter2'),
+  ];
 
   @override
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
@@ -152,20 +172,516 @@ class LibraryElementImpl implements LibraryElement {
   LibraryElementImpl(this.name);
 
   @override
-  List<ElementAnnotation> get metadata => [];
+  Metadata get metadata => MetadataImpl();
 
   @override
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
-class PropertyAccessorElementImpl implements PropertyAccessorElement {
+class GetterElementImpl implements GetterElement {
   @override
   final String name;
-  @override
-  final bool isSetter;
 
-  PropertyAccessorElementImpl(this.name, this.isSetter);
+  GetterElementImpl(this.name);
 
   @override
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+  Metadata get metadata => MetadataImpl();
+
+  @override
+  T? accept<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement accept
+    throw UnimplementedError();
+  }
+
+  @override
+  T? accept2<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement accept2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement baseElement
+  GetterElement get baseElement => throw UnimplementedError();
+
+  @override
+  // TODO: implement children
+  List<Element> get children => throw UnimplementedError();
+
+  @override
+  // TODO: implement children2
+  List<Element> get children2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement correspondingSetter
+  SetterElement? get correspondingSetter => throw UnimplementedError();
+
+  @override
+  // TODO: implement correspondingSetter2
+  SetterElement? get correspondingSetter2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement displayName
+  String get displayName => throw UnimplementedError();
+
+  @override
+  String displayString({bool multiline = false, bool preferTypeAlias = false}) {
+    // TODO: implement displayString
+    throw UnimplementedError();
+  }
+
+  @override
+  String displayString2({
+    bool multiline = false,
+    bool preferTypeAlias = false,
+  }) {
+    // TODO: implement displayString2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement documentationComment
+  String? get documentationComment => throw UnimplementedError();
+
+  @override
+  // TODO: implement enclosingElement
+  Element get enclosingElement => throw UnimplementedError();
+
+  @override
+  // TODO: implement enclosingElement2
+  Element get enclosingElement2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement firstFragment
+  GetterFragment get firstFragment => throw UnimplementedError();
+
+  @override
+  // TODO: implement formalParameters
+  List<FormalParameterElement> get formalParameters =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement fragments
+  List<GetterFragment> get fragments => throw UnimplementedError();
+
+  @override
+  String getExtendedDisplayName({String? shortName}) {
+    // TODO: implement getExtendedDisplayName
+    throw UnimplementedError();
+  }
+
+  @override
+  String getExtendedDisplayName2({String? shortName}) {
+    // TODO: implement getExtendedDisplayName2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement hasImplicitReturnType
+  bool get hasImplicitReturnType => throw UnimplementedError();
+
+  @override
+  // TODO: implement id
+  int get id => throw UnimplementedError();
+
+  @override
+  // TODO: implement isAbstract
+  bool get isAbstract => throw UnimplementedError();
+
+  @override
+  bool isAccessibleIn(LibraryElement library) {
+    // TODO: implement isAccessibleIn
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isAccessibleIn2(LibraryElement library) {
+    // TODO: implement isAccessibleIn2
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isDeprecatedWithKind(String kind) {
+    // TODO: implement isDeprecatedWithKind
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement isExtensionTypeMember
+  bool get isExtensionTypeMember => throw UnimplementedError();
+
+  @override
+  // TODO: implement isExternal
+  bool get isExternal => throw UnimplementedError();
+
+  @override
+  // TODO: implement isPrivate
+  bool get isPrivate => throw UnimplementedError();
+
+  @override
+  // TODO: implement isPublic
+  bool get isPublic => throw UnimplementedError();
+
+  @override
+  // TODO: implement isSimplyBounded
+  bool get isSimplyBounded => throw UnimplementedError();
+
+  @override
+  // TODO: implement isStatic
+  bool get isStatic => throw UnimplementedError();
+
+  @override
+  // TODO: implement isSynthetic
+  bool get isSynthetic => throw UnimplementedError();
+
+  @override
+  // TODO: implement kind
+  ElementKind get kind => throw UnimplementedError();
+
+  @override
+  // TODO: implement library
+  LibraryElement get library => throw UnimplementedError();
+
+  @override
+  // TODO: implement library2
+  LibraryElement get library2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement lookupName
+  String? get lookupName => throw UnimplementedError();
+
+  @override
+  // TODO: implement metadata2
+  Metadata get metadata2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement name3
+  String? get name3 => throw UnimplementedError();
+
+  @override
+  // TODO: implement nonSynthetic
+  Element get nonSynthetic => throw UnimplementedError();
+
+  @override
+  // TODO: implement nonSynthetic2
+  Element get nonSynthetic2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement returnType
+  DartType get returnType => throw UnimplementedError();
+
+  @override
+  // TODO: implement session
+  AnalysisSession? get session => throw UnimplementedError();
+
+  @override
+  // TODO: implement sinceSdkVersion
+  Version? get sinceSdkVersion => throw UnimplementedError();
+
+  @override
+  Element? thisOrAncestorMatching(bool Function(Element p1) predicate) {
+    // TODO: implement thisOrAncestorMatching
+    throw UnimplementedError();
+  }
+
+  @override
+  Element? thisOrAncestorMatching2(bool Function(Element p1) predicate) {
+    // TODO: implement thisOrAncestorMatching2
+    throw UnimplementedError();
+  }
+
+  @override
+  E? thisOrAncestorOfType<E extends Element>() {
+    // TODO: implement thisOrAncestorOfType
+    throw UnimplementedError();
+  }
+
+  @override
+  E? thisOrAncestorOfType2<E extends Element>() {
+    // TODO: implement thisOrAncestorOfType2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement type
+  FunctionType get type => throw UnimplementedError();
+
+  @override
+  // TODO: implement typeParameters
+  List<TypeParameterElement> get typeParameters => throw UnimplementedError();
+
+  @override
+  // TODO: implement typeParameters2
+  List<TypeParameterElement> get typeParameters2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement variable
+  PropertyInducingElement get variable => throw UnimplementedError();
+
+  @override
+  // TODO: implement variable3
+  PropertyInducingElement? get variable3 => throw UnimplementedError();
+
+  @override
+  void visitChildren<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement visitChildren
+  }
+
+  @override
+  void visitChildren2<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement visitChildren2
+  }
+}
+
+class SetterElementImpl implements SetterElement {
+  @override
+  final String name;
+
+  SetterElementImpl(this.name);
+
+  @override
+  Metadata get metadata => MetadataImpl();
+
+  @override
+  T? accept<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement accept
+    throw UnimplementedError();
+  }
+
+  @override
+  T? accept2<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement accept2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement baseElement
+  SetterElement get baseElement => throw UnimplementedError();
+
+  @override
+  // TODO: implement children
+  List<Element> get children => throw UnimplementedError();
+
+  @override
+  // TODO: implement children2
+  List<Element> get children2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement correspondingGetter
+  GetterElement? get correspondingGetter => throw UnimplementedError();
+
+  @override
+  // TODO: implement correspondingGetter2
+  GetterElement? get correspondingGetter2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement displayName
+  String get displayName => throw UnimplementedError();
+
+  @override
+  String displayString({bool multiline = false, bool preferTypeAlias = false}) {
+    // TODO: implement displayString
+    throw UnimplementedError();
+  }
+
+  @override
+  String displayString2({
+    bool multiline = false,
+    bool preferTypeAlias = false,
+  }) {
+    // TODO: implement displayString2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement documentationComment
+  String? get documentationComment => throw UnimplementedError();
+
+  @override
+  // TODO: implement enclosingElement
+  Element get enclosingElement => throw UnimplementedError();
+
+  @override
+  // TODO: implement enclosingElement2
+  Element get enclosingElement2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement firstFragment
+  SetterFragment get firstFragment => throw UnimplementedError();
+
+  @override
+  // TODO: implement formalParameters
+  List<FormalParameterElement> get formalParameters =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement fragments
+  List<SetterFragment> get fragments => throw UnimplementedError();
+
+  @override
+  String getExtendedDisplayName({String? shortName}) {
+    // TODO: implement getExtendedDisplayName
+    throw UnimplementedError();
+  }
+
+  @override
+  String getExtendedDisplayName2({String? shortName}) {
+    // TODO: implement getExtendedDisplayName2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement hasImplicitReturnType
+  bool get hasImplicitReturnType => throw UnimplementedError();
+
+  @override
+  // TODO: implement id
+  int get id => throw UnimplementedError();
+
+  @override
+  // TODO: implement isAbstract
+  bool get isAbstract => throw UnimplementedError();
+
+  @override
+  bool isAccessibleIn(LibraryElement library) {
+    // TODO: implement isAccessibleIn
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isAccessibleIn2(LibraryElement library) {
+    // TODO: implement isAccessibleIn2
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isDeprecatedWithKind(String kind) {
+    // TODO: implement isDeprecatedWithKind
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement isExtensionTypeMember
+  bool get isExtensionTypeMember => throw UnimplementedError();
+
+  @override
+  // TODO: implement isExternal
+  bool get isExternal => throw UnimplementedError();
+
+  @override
+  // TODO: implement isPrivate
+  bool get isPrivate => throw UnimplementedError();
+
+  @override
+  // TODO: implement isPublic
+  bool get isPublic => throw UnimplementedError();
+
+  @override
+  // TODO: implement isSimplyBounded
+  bool get isSimplyBounded => throw UnimplementedError();
+
+  @override
+  // TODO: implement isStatic
+  bool get isStatic => throw UnimplementedError();
+
+  @override
+  // TODO: implement isSynthetic
+  bool get isSynthetic => throw UnimplementedError();
+
+  @override
+  // TODO: implement kind
+  ElementKind get kind => throw UnimplementedError();
+
+  @override
+  // TODO: implement library
+  LibraryElement get library => throw UnimplementedError();
+
+  @override
+  // TODO: implement library2
+  LibraryElement get library2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement lookupName
+  String? get lookupName => throw UnimplementedError();
+
+  @override
+  // TODO: implement metadata2
+  Metadata get metadata2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement name3
+  String? get name3 => throw UnimplementedError();
+
+  @override
+  // TODO: implement nonSynthetic
+  Element get nonSynthetic => throw UnimplementedError();
+
+  @override
+  // TODO: implement nonSynthetic2
+  Element get nonSynthetic2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement returnType
+  DartType get returnType => throw UnimplementedError();
+
+  @override
+  // TODO: implement session
+  AnalysisSession? get session => throw UnimplementedError();
+
+  @override
+  // TODO: implement sinceSdkVersion
+  Version? get sinceSdkVersion => throw UnimplementedError();
+
+  @override
+  Element? thisOrAncestorMatching(bool Function(Element p1) predicate) {
+    // TODO: implement thisOrAncestorMatching
+    throw UnimplementedError();
+  }
+
+  @override
+  Element? thisOrAncestorMatching2(bool Function(Element p1) predicate) {
+    // TODO: implement thisOrAncestorMatching2
+    throw UnimplementedError();
+  }
+
+  @override
+  E? thisOrAncestorOfType<E extends Element>() {
+    // TODO: implement thisOrAncestorOfType
+    throw UnimplementedError();
+  }
+
+  @override
+  E? thisOrAncestorOfType2<E extends Element>() {
+    // TODO: implement thisOrAncestorOfType2
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement type
+  FunctionType get type => throw UnimplementedError();
+
+  @override
+  // TODO: implement typeParameters
+  List<TypeParameterElement> get typeParameters => throw UnimplementedError();
+
+  @override
+  // TODO: implement typeParameters2
+  List<TypeParameterElement> get typeParameters2 => throw UnimplementedError();
+
+  @override
+  // TODO: implement variable
+  PropertyInducingElement get variable => throw UnimplementedError();
+
+  @override
+  // TODO: implement variable3
+  PropertyInducingElement? get variable3 => throw UnimplementedError();
+
+  @override
+  void visitChildren<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement visitChildren
+  }
+
+  @override
+  void visitChildren2<T>(ElementVisitor2<T> visitor) {
+    // TODO: implement visitChildren2
+  }
 }
